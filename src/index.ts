33c1803258879eb8483d9ed897f50aeed07ab773
nj <niranjan.devasani@gmail.com>
2024-10-16 12:05:44 +0530

    Add sitemaper

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('sitemaper')
+  .description('Simple tool for generating sitemaps for your website.')
+  .version('1.0.0');
+
+program
+  .option('-w, --website <name>', 'The name of the website.')
+  .option('-d, --depth <message>', 'Depth of the website to crawl.')
+  .option('-o, --output <message>', 'Output path of the sitemap.xml')
+  .action((options) => {
+    const website = options.website || 'https://www.example.com';
+    const depth = options.depth || 10;
+    const output = options.output || './sitemap.xml';
+    console.log({website, depth, output});
+  });
+
+program.parse(process.argv);
